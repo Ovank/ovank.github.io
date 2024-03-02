8fc321ab8462cabbe0e3a3a4357590d67b599ece
Ovank <omvats89@gmail.com>
2024-03-02 12:40:34 +0530

    modified code to test for responsive layout

--- home/home.js
+++ home/home.js
@@ -8,11 +8,11 @@ function homePageContain(){
     var homePage_html=`
     <div class="Home">
         <div class="hero">
-            <span><img src="../assets/profile.png" style="width:400px;height:400px"></span>
+            <span class="profile_img"><img src="../assets/profile.png"></span>
             <h1>Hello <span>&#128075;</span>, I am Om Vats.<span>&#128522;</span></h1>
             <h2>I am a software Development Engineer <span>&#128640;</span></h2>
         </div>
-        <div>
+        <div class="quote">
           <h4>
             <blockquote>
             Life is like riding a <span  style='font-size:50px;'>&#128690;</span>. 
